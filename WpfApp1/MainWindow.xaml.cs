201f4e18912208a06643e81b1036fa673a3a7d1f
kmcmanus <mcmanus.keith@gmail.com>
2020-03-24 13:37:58 +0000

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            imgBig.Source = new BitmapImage(new Uri("cards.png", UriKind.Relative));
+
+            Bitmap cards = new Bitmap(@"C:\code\repos\ConsoleApp4\WpfApp1\cards.png");
+
+            System.Drawing.Rectangle srcRect = new System.Drawing.Rectangle(0, 0, 73, 98);
+            Bitmap card = (Bitmap)cards.Clone(srcRect, cards.PixelFormat);
+
+            imgSmall.Source = Convert(card);
+        }
+
+        public BitmapImage Convert(System.Drawing.Image img)
+        {
+            using (var memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+    }
+}
